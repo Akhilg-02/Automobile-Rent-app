2149072cf8bd18f13a2baf824090cf092098ed2a
unknown <guptaakhil0206@gmail.com>
2024-04-07 16:56:04 +0530

    modified the design for navigation bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
-import NavigationBar from "./components/Dashbord/NavigationBar";
 import Footer from "./components/Dashbord/Footer";
 import { MainRoutes } from "./Routes/MainRoutes";
-import SampleNav from "./components/Dashbord/sampleNav";
+import NavigationBar from "./components/Dashbord/NavigationBar";
 
 function App() {
   return (
     <div className="App">
       {/* <NavigationBar /> */}
-      <SampleNav/>
+      <NavigationBar/>
       <MainRoutes/>
       <Footer/>
     </div>
