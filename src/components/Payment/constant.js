8db4e34512176b6bfbf99f43baca28780e00533c
unknown <guptaakhil0206@gmail.com>
2024-06-15 01:19:29 +0530

    updated the configuration of application

---
+++ src/components/Payment/constant.js
@@ -0,0 +1,16 @@
+//Razor Pay
+export const Razor_API_KEY = process.env.RAZOR_API_KEY;
+//EmailJS
+export const email_ServiceId = process.env.EMAIL_SERVICE_ID;
+export const email_TemplateId = process.env.EMAIL_TEMPLATE_ID;
+export const email_PublicKey = process.env.EMAIL_PUBLIC_KEY;
+export const emailJs_Api = process.env.EMAIL_API;
+//FireBase
+export const fire_apiKey = process.env.FIREBASE_API_KEY;
+export const fire_appId = process.env.FIREBASE_APP_ID;
+export const fire_authDomain = process.env.FIREBASE_AUTH_DOMAIN;
+export const fire_storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+export const fire_projectId = process.env.FIREBASE_PROJECT_ID;
+export const fire_messagingSenderId = process.env.FIREBASE_MESSAGE_ID;
+export const fire_measurementId =  process.env.FIREBASE_MEASURE_ID;
+
