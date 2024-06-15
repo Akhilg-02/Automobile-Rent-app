c3bc359ec02e64a4ce0a3bde8b5021cd906c60d7
unknown <guptaakhil0206@gmail.com>
2024-06-16 00:26:04 +0530

    added

--- src/components/Payment/constant.js
+++ src/components/Payment/constant.js
@@ -1,6 +1,15 @@
-
-
-export const fire_projectId = 'vechile-service';
-export const fire_messagingSenderId =  "589116394993";
-export const fire_measurementId =  "G-5G8KDG58ZQ"
-
+//Razor Pay
+export const Razor_API_KEY = process.env.RAZOR_API_KEY;
+//EmailJS
+export const email_ServiceId = process.env.EMAIL_SERVICE_ID;
+export const email_TemplateId = process.env.EMAIL_TEMPLATE_ID;
+export const email_PublicKey = process.env.EMAIL_PUBLIC_KEY;
+export const emailJs_Api = process.env.EMAIL_API;
+//FireBase
+export const fire_apiKey = process.env.FIREBASE_API_KEY;
+export const fire_appId = process.env.FIREBASE_APP_ID;
+export const fire_authDomain = process.env.FIREBASE_AUTH_DOMAIN;
+export const fire_storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+export const fire_projectId = process.env.FIREBASE_PROJECT_ID;
+export const fire_messagingSenderId = process.env.FIREBASE_MESSAGE_ID;
+export const fire_measurementId =  process.env.FIREBASE_MEASURE_ID;
