43acd490d5d491e8837f8f6b9e78c46737b23356
unknown <guptaakhil0206@gmail.com>
2024-06-16 00:03:46 +0530

    added

--- commit.sh
+++ commit.sh
@@ -8,7 +8,7 @@ git add --all
 git status
 
 # Commit changes with a specified message
-git commit -m "removed file from git ignore file"
+git commit -m "added"
 
 # Push changes to the remote repository
 git push origin main
